a1d7e0f64c8b5ec52657b73d316b274e3e33b739
James Thomas <jamesjoethomas@gmail.com>
2019-11-28 12:07:49 -0800

    error message from verifier

--- src/main/java/edu/stanford/fleet/rw/VerifyClearIntTiles.java
+++ src/main/java/edu/stanford/fleet/rw/VerifyClearIntTiles.java
@@ -17,6 +17,7 @@ public class VerifyClearIntTiles {
         String dir = "."; // "/home/jamestho/floorplanning";
         String pblockStr = new String(Files.readAllBytes(Paths.get(dir + "/if_shell_exclude.pblock")), StandardCharsets.UTF_8);
         PBlock pblock = new PBlock(Device.getDevice(Device.AWS_F1), pblockStr.trim());
+        boolean foundPip = false;
         for (Net n : shell.getNets()) {
             if (n.getName().contains("/ifn") || n.isStaticNet()) {
                 continue;
@@ -24,9 +25,14 @@ public class VerifyClearIntTiles {
             for (PIP p : n.getPIPs()) {
                 if (pblock.containsTile(p.getTile())) {
                     System.out.println(n.getName() + " " + p.getTile().getName());
+                    foundPip = true;
                 }
             }
         }
-        System.out.println("Verification of " + shellPath + " finished...");
+        if (!foundPip) {
+            System.out.println("Verification of " + shellPath + " finished...");
+        } else {
+            System.out.println("ERROR: Verification of " + shellPath + " finished with errors, see above");
+        }
     }
 }
